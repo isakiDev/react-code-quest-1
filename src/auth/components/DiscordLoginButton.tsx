c8c30b4ed7a66fff0fa7b12f2e670f544bf43137
isakiDev <gaspar.c.developer@gmail.com>
2024-03-12 04:04:49 -0300

    feat: add button to test OAuth2

---
+++ src/auth/components/DiscordLoginButton.tsx
@@ -0,0 +1,23 @@
+const DiscordLoginButton = () => {
+  const handleLogin = () => {
+    // URL de autorización de Discord
+    // const authorizationUrl = 'https://discord.com/oauth2/authorize' +
+    //   '?client_id=1216779662492041277' +
+    //   '&redirect_uri=http://localhost:3000/api/auth' +
+    //   '&response_type=token' +
+    //   '&scope=guilds+identify';
+
+    const authorizationUrl = 'http://localhost:3000/api/auth'
+
+    // Redirigir al usuario a Discord para iniciar sesión
+    window.location.href = authorizationUrl;
+  };
+
+  return (
+    <button onClick={handleLogin}>
+      Iniciar sesión con Discord
+    </button>
+  );
+};
+
+export default DiscordLoginButton;
